45b0e83afa4e4749e7d8c54909fed8fa0aad2041
joykbiswas <joykumarbiswas100@gmail.com>
2024-10-02 21:15:43 +0600

    Mongodb connection setup database

---
+++ index.js
@@ -0,0 +1,64 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+
+require("dotenv").config();
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+
+const port = process.env.PORT || 5000;
+
+//middleware
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.cqpfzla.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+    const blogCollection = client.db("nextBlogDB").collection("Blog");
+
+    const userCollection = client.db("nextBlogDB").collection("user");
+
+    //create
+    app.post("/addBlog", async (req, res) => {
+      const newBlog = req.body;
+      console.log(newBlog);
+      const result = await blogCollection.insertOne(newBlog);
+      res.send(result);
+    });
+
+    app.get('/addBlog', async(req, res) =>{
+        const cursor = blogCollection.find()
+        const result = await cursor.toArray()
+        res.send(result)
+    })
+
+
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("NextBlog server is running");
+});
+app.listen(port, () => {
+  console.log(`NextBlog server is running on port:${port}`);
+});

514e2cbee4953bb9d55f39264e5b41f4ed0e56cf
joykbiswas <joykumarbiswas100@gmail.com>
2024-10-05 20:28:02 +0600

    deploy vercel

--- index.js
+++ index.js
@@ -8,7 +8,13 @@ const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const port = process.env.PORT || 5000;
 
 //middleware
-app.use(cors());
+app.use(cors(
+  {
+    origin: ["https://next-blog-frontend-vert.vercel.app",],
+    Credential: true,
+    optionSuccessStatus: 200,
+  }
+));
 app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.cqpfzla.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
